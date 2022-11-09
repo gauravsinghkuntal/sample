86d1692b5fb4fe325c64e557195944f2766aba21
gauravsinghkuntal <9555644995gaurav@gmail.com>
2022-11-09 13:22:46 +0530

    changes made to tester class

--- TestApp/src/com/ineuron/ai/Tester.java
+++ TestApp/src/com/ineuron/ai/Tester.java
@@ -7,6 +7,12 @@ public class Tester {
 
 		String a="gaurav singh kuntal";
 		System.out.println("My name is Gaurav Singh Kuntal ");
+		int[] b= {10,20,30,40};
+		for (int i : b) {
+			System.out.print("Elements of the array are " + i);
+			System.out.print(i + " ");
+		}
+		
 	}
 
 }
