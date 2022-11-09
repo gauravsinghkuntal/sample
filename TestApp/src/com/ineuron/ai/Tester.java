7daee55883497370bd3c20ca60bc5bad8da12444
gauravsinghkuntal <9555644995gaurav@gmail.com>
2022-11-09 13:14:59 +0530

    first commit

---
+++ TestApp/src/com/ineuron/ai/Tester.java
@@ -0,0 +1,12 @@
+package com.ineuron.ai;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String a="gaurav singh kuntal";
+		System.out.println("My name is Gaurav Singh Kuntal ");
+	}
+
+}
